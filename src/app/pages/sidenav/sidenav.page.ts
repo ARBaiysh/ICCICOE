527f7bc0aeb844bb046983188e108a5a61b0c8cc
ARBaiysh <o0052i@mail.ru>
2022-07-26 21:19:25 +0600

    edit router pages

--- src/app/pages/sidenav/sidenav.page.ts
+++ src/app/pages/sidenav/sidenav.page.ts
@@ -5,6 +5,7 @@ import {Observable} from 'rxjs';
 import {CurrentUserInterface} from '../../users/types/currentUser.interface';
 import {currentUserSelector, isLoggedInSelector, isSubmittingSelector} from '../../users/store/selectors';
 import {getCurrentUserAction} from '../../users/store/actions/getCurrentUser.action';
+import {Router, RouterEvent} from '@angular/router';
 
 @Component({
     selector: 'app-sidenav',
@@ -16,7 +17,30 @@ export class SidenavPage implements OnInit {
     isSubmitting$: Observable<boolean>;
     isLoggedIn$: Observable<boolean | null>;
 
-    constructor(private store: Store) {
+    active = '';
+
+    navs = [
+        {
+            name: 'About',
+            link: '/nav/about',
+            icon: 'person-circle'
+        },
+        {
+            name: 'Blog',
+            link: '/nav/blog',
+            icon: 'albums'
+        },
+        {
+            name: 'Contact',
+            link: '/nav/contact',
+            icon: 'call'
+        }
+    ];
+
+    constructor(private store: Store, private router: Router ) {
+        this.router.events.subscribe(( event: RouterEvent ) => {
+            this.active = event.url;
+        });
     }
 
     onExit(): void {
