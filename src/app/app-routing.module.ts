0afec5dce562bec54a42bb6b058b52289db7313d
ARBaiysh <o0052i@mail.ru>
2022-07-27 15:53:04 +0600

    add register.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 
 const routes: Routes = [
-  {path: '', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)},
+    {path: '', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)},
+    {path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterPageModule)}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules, useHash: true})],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules, useHash: true})],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
--- src/app/pages/users-list/users-list.module.ts
+++ src/app/pages/users-list/users-list.module.ts
@@ -1,22 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { UsersListPageRoutingModule } from './users-list-routing.module';
+import {UsersListPageRoutingModule} from './users-list-routing.module';
 
-import { UsersListPage } from './users-list.page';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import {UsersListPage} from './users-list.page';
+import {Ng2SearchPipeModule} from 'ng2-search-filter';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    UsersListPageRoutingModule,
-    Ng2SearchPipeModule
-  ],
-  declarations: [UsersListPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        UsersListPageRoutingModule,
+        Ng2SearchPipeModule,
+    ],
+    declarations: [UsersListPage]
 })
-export class UsersListPageModule {}
+export class UsersListPageModule {
+}
